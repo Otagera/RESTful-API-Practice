1bff409fc66537defb1dff31b32d97a685861265
Otagera <Otagera@gmail.com>
2020-09-21 12:18:45 +0100

    Uncommenting preveenting CUD and reset

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,11 +4,11 @@ var router = express.Router();
 let countryCtrl = require("../controllers/countries");
 
 let apiGuard = (req, res, next)=>{
-	// if(res.get('host') !== 'localhost:3000'){
-		// res.statusJson(404, { error: 'Cannot Create, Update or Delete countries from the API while in production'});
-	// } else {
+	if(res.get('host') !== 'localhost:3000'){
+		res.statusJson(404, { error: 'Cannot Create, Update or Delete countries from the API while in production'});
+	} else {
 		next();
-	// }
+	}
 }
 
 router.route('/countries/:countryid')
